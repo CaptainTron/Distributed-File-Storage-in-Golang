6b0c6051b09ed6b3cc7997cd96a42ce577a3b80a
Vaibhav Yadav <tron21vaibhav@gmail.com>
2024-06-15 16:24:32 +0530

    added TCPPeer connectionn

---
+++ p2p/transport.go
@@ -0,0 +1,12 @@
+package p2p
+
+// it represents the connection b/w nodes
+type Peer interface{
+	
+}
+
+
+// it will handle the connection between any nodes, like TCP, UDP and websockets
+type Transport interface{
+
+}
